a349a46f1654bc8dee6d6995425389858c8a27b2
ggroshansii <garthgroshansii@gmail.com>
2021-10-12 15:36:56 -0400

    added GET/POST view in order app

--- order/views.py
+++ order/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import OrderItem
+from serializers import OrderItemSerializer
+
 
 # Create your views here.
+
+class OrderItemListAPIView (generics.ListCreateAPIView):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
